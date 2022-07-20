f7f2a9f32a7c5b30b10476c46f843273a9a267a1
Casley <Casleynkosi@gmail.com>
2022-07-20 17:43:39 +0000

    Task 4

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -4,19 +4,24 @@
 
 class Square:
     """Represents a square
+    
     Attributes:
         __size (int): size of a side of the square
     """
     def __init__(self, size=0):
         """initializes the square
+        
         Args:
             size (int): size of a side of the square
+        
         Returns:
             None
         """
         self.size = size
+    
     def area(self):
         """calculates the square's area
+        
         Returns:
             The area of the square
         """
